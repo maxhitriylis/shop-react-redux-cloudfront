c40f005aceffbcd5b1a5247de515df1d04229666
Maksim <orpshawn@gmail.com>
2022-01-16 16:01:53 +0300

    added file upload feature with s3

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://d0nm3scxn9.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://6298mj1uic.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://d0nm3scxn9.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
